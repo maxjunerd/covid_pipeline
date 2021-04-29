c35df95946c6225710a8c472e2327d912f2d8e90
Chayanon Tonsai <maxjunerd@gmail.com>
2021-04-30 03:43:05 +0700

    Update covid_pipeline.py
    change how to use datetime

--- covid_pipeline.py
+++ covid_pipeline.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import requests
-import datetime
+from datetime import date, datetime, timedelta
 import pendulum 
 from google.cloud import storage
 
@@ -20,7 +20,7 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 2,
-    "retry_delay": datetime.timedelta(minutes=60)
+    "retry_delay": timedelta(minutes=60)
 }
 
 # Get COVID data from Thai Department of Disease Control and save to Google Storage
@@ -30,19 +30,21 @@ def get_data(bucket):
     print(bucket)
     print(df)
 
-
 with DAG(
     "covid_pipeline",
     default_args=default_args,
     description="Thailand COVID Data Pipeline",
-    schedule_interval=datetime.timedelta(days=1),
-    start_date=datetime.datetime(2021, 4, 29, tzinfo=bangkok_tz),
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2021, 4, 29, tzinfo=bangkok_tz),
     tags=["pipeline", "covid"],
 ) as dag:
 
     get_data_task = PythonOperator(
         task_id="get_data",
+        provide_context=True,
         python_callable=get_data,
         op_kwargs={"bucket": bucket}
     )
+
+    get_data_task
     
